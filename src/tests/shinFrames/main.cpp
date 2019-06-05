7a7c715376403c5aa7144ceff451d500b2dde860
LoganTann <logon313@hotmail.fr>
2019-06-05 17:27:55 +0200

    Test : Shintaro can now run R/L

--- src/tests/shinFrames/main.cpp
+++ src/tests/shinFrames/main.cpp
@@ -2,45 +2,92 @@
 #include <SFML/Graphics.hpp>
 #include <SFML/Window.hpp>
 #include <SFML/Audio.hpp>
+#include "../../easing.h"
 
-int main(){
-  sf::RenderWindow scr(sf::VideoMode(620,280), "Test shintaro ruuunnn");
-  scr.setFramerateLimit(60);
-
-  sf::Clock clock;
-  sf::Time newTime, oldTime;
-  oldTime = clock.getElapsedTime();
-  sf::Texture perso;
-    //if (!perso.loadFromFile("resized.png")) return 0;
-    if (!perso.loadFromFile("test.png")) return 0;
-    int x = 0, frameN = 18, frameW = 170, frameH = 280;
-  sf::Sprite sprite_perso;
-    sprite_perso.setTexture(perso);
-    sprite_perso.setPosition(200,0);
-    sprite_perso.setTextureRect(sf::IntRect(0,0,frameW,frameH));
-  while (scr.isOpen()) {
-      sf::Event event;
-      while (scr.pollEvent(event)) {switch (event.type){
-        case sf::Event::Closed: scr.close(); break;
-        case sf::Event::KeyReleased :
-          if (event.key.code == sf::Keyboard::Return) {
-            std::cout<<"return";
-          }
-          break;
-        default:break;
-      }}
+class MapEntity{
+  public:
+    MapEntity(){
+      x = animInit = animDist = 0;
+      frameN = 18;
+      frameW = 170;
+      frameH = 280;
+      spritePosX = 0;
+      spritePosY = 680;
+      animatingX = animatingY = false;
+      spriteTxt.loadFromFile("test.png");
+      oldTime = clock.getElapsedTime();
+      sprite.setTexture(spriteTxt);
+      sprite.setOrigin(170/2,280);
+      sprite.setPosition(spritePosX,spritePosY);
+      sprite.setTextureRect(sf::IntRect(0,0,frameW,frameH));
+    };
+    bool draw(sf::RenderWindow& scr){
+      const float getms = clock.getElapsedTime().asSeconds() - animStart.asSeconds();
+      if (animatingX)
+        spritePosX = animInit + animDist * getms;
+      if (animatingY)
+        spritePosY = animInit + animDist * getms;
+      sprite.setPosition(spritePosX,spritePosY);
       newTime = clock.getElapsedTime();
-      if (newTime>oldTime+sf::milliseconds(40)){
+      if (newTime>oldTime+sf::milliseconds(50)){
         if (x>=frameN)
           x=0;
-        //sprite_perso.setTextureRect(sf::IntRect(120*x,0,120,198));
-        sprite_perso.setTextureRect(sf::IntRect(frameW*x,0,frameW,frameH));
+        //sprite.setTextureRect(sf::IntRect(120*x,0,120,198));
+        sprite.setTextureRect(sf::IntRect(frameW*x,0,frameW,frameH));
         oldTime = newTime;
         x++;
       }
+      scr.draw(sprite);
+      return 0;
+    };
+    bool animate(int direction){
+      animatingX = true;
+      animStart = clock.getElapsedTime();
+      switch (direction) {
+      case sf::Keyboard::Right :{
+        sprite.setScale(1,1);
+        animInit = spritePosX;
+        animDist = 280;
+        break; }
+      case sf::Keyboard::Left :{
+        sprite.setScale(-1,1);
+        animInit = spritePosX;
+        animDist = -280;
+        break;}
+      case sf::Keyboard::Up : {spritePosY -= 10;break;}
+      case sf::Keyboard::Down : {spritePosY += 10;break;}
+      }
+      return true;
+    }
+    bool animatingX, animatingY;
+  private :
+    sf::Texture spriteTxt;
+    sf::Clock clock;
+    sf::Time newTime, oldTime, animStart;
+    sf::Sprite sprite;
+    int x, frameN, frameW, frameH, spritePosX, spritePosY,
+      animInit, animDist;
+};
 
+int main(){
+  sf::RenderWindow scr(sf::VideoMode(1280,720), "Test shintaro ruuunnn");
+  scr.setFramerateLimit(60);
+  MapEntity sprite;
+  while (scr.isOpen()) {
+      sf::Event event;
+      while (scr.pollEvent(event)) {
+        switch (event.type){
+          case sf::Event::Closed: scr.close(); break;
+          case sf::Event::KeyPressed:
+            if (!sprite.animatingX && event.key.code >= 71 && event.key.code <= 74)
+              sprite.animate(event.key.code);
+            break;
+          case sf::Event::KeyReleased:
+            sprite.animatingX = false;
+          default:break; }
+      }
       scr.clear(sf::Color::White);
-      scr.draw(sprite_perso);
+      sprite.draw(scr);
       scr.display();
   }
 
