b8c912c40c7d21fe68b382cc50165e7d434f72e3
ShinProg (Logan Tann) <logon313@hotmail.fr>
2019-10-23 18:41:51 +0200

    Updated the shinOs window

--- js/addons/shinOS/main.js
+++ js/addons/shinOS/main.js
@@ -1,7 +1,16 @@
 /* Shintaro OS. Part of the Retaining's memories fangame
  * Copyright (c) 2017-2019 ShinProg / Kagescan
 */
-
+function isDescendantOfDatasetType(name, child) {
+     var node = child.parentNode;
+     while (node != null) {
+         if (node.dataset.type == name) {
+             return true;
+         }
+         node = node.parentNode;
+     }
+     return false;
+}
 let shinOSinstance = {
 	run(){
 		this.container = document.getElementById("gamePlugins");
@@ -9,8 +18,8 @@ let shinOSinstance = {
 		this.container.classList.remove("hide");
 		this.container.innerHTML = `
 		<div id='shinOsWinContainer'>
-			<div id='shinOsDesktop'></div>
 			<div id='shinOsWindows'></div>
+			<div id='shinOsDesktop'></div>
 		</div>
 		<div id='shinOsBottomBar'>
 			<div id='shinOsStartMenu'>Shintaro</div>
@@ -18,16 +27,14 @@ let shinOSinstance = {
 			<div id='shinOsClock'>8:30<br>08/15/20XX</div>
 		</div>
 		`;
-	this.allCallbacks = [];
-		this.windowContainer = document.getElementById("shinOsWinContainer");
+		/*this.allCallbacks = [];
 		document.getElementById("shinOsDesktop").addEventListener("click", function(c){
 			let e = c.target;
-			if (e.dataset.type=="icon"){
-				console.log(shinOSinstance.allCallbacks, parseInt(e.dataset.i))
+			if (isDescendantOfDatasetType(e,"icon")){
 				shinOSinstance.allCallbacks[parseInt(e.dataset.i)]();
 				//shinOSinstance.addWindow(e.dataset.title);
 			}
-		});
+		});*/
 		console.log("shinOs démarré");
 	},
 	toggle(){
@@ -39,20 +46,38 @@ let shinOSinstance = {
 		this.container.innerHTML = "";
 		console.log("ShinOS éteint");
 	},
-	addWindow(t){
-		console.log(t);
-		/*desk = this.windowContainer;
-		var newDiv = document.createElement("div");*/
+	addWindow(title, content, icon){
+		let win = document.createElement("div"), taskIcon = document.createElement("div");
+		win.classList.add("active");
+		win.innerHTML = `
+		<div class='title'>
+			<a href='#'><i class="fas fa-times-circle"></i></a>
+			<span>${title}</span>
+			<i class="fas ${icon}"></i>
+		</div>
+		<div class='content'>${content}</div>`;
+		win.firstElementChild.firstElementChild.addEventListener('click', ()=>{ shinOSinstance.closeWindow(title); });
+		taskIcon.innerHTML = `<i class="fas ${icon}"></i>`;
+		taskIcon.classList.add("active");
+		taskIcon.dataset.title = win.dataset.title = title;
+		taskIcon.addEventListener('click', ()=>{ console.log("foobar") });
+		document.getElementById("shinOsWindows").appendChild(win);
+		document.getElementById("shinOsAppsNav").appendChild(taskIcon);
+	},
+	closeWindow(title){
+		console.log(`#shinOsAppsNav [data-title=${title}]`);
+		document.querySelector(`#shinOsAppsNav div[data-title=${title}]`).remove();
+		document.querySelector(`#shinOsWindows div[data-title=${title}]`).remove();
 	},
 	addApp(title, icon="fa-file-image", callback){
 		let desktopIcons = document.getElementById("shinOsDesktop");
 		let newIcon = document.createElement("div");
-		let index = this.allCallbacks.length;
+		//let index = this.allCallbacks.length;
 		newIcon.innerHTML = `<i class="fas ${icon}"></i><span>${title}</span>`;
-		newIcon.dataset.title = title;
-		newIcon.dataset.type = "icon";
-		newIcon.dataset.i = index;
+		//newIcon.dataset.title = title;
+		//newIcon.dataset.type = "icon";
+		//newIcon.dataset.i = index;
 		desktopIcons.appendChild(newIcon);
-		this.allCallbacks[index] = callback;
+		newIcon.onclick = callback;
 	}
 }
--- js/chapters/chap1.js
+++ js/chapters/chap1.js
@@ -3,7 +3,16 @@ script["chapter01-start"] = [
 	function(){
 		shinOSinstance.run();
 		shinOSinstance.addApp("Dire bonjour !", "fa-comments", function(){
-			alert("COUCOU")
+			alert("HELLO !!")
+		});
+		shinOSinstance.addApp("vocaloid", "fa-guitar", function(){
+			shinOSinstance.addWindow("vocaloid", "ça bug (?)", "fa-guitar");
+		});
+		shinOSinstance.addApp("jambes", "fa-folder-open", function(){
+			let valeur = prompt("Mot de passe ?");
+			if (valeur == "4510471"){
+				shinOSinstance.addWindow("jambes", "empty... ?", "fa-folder-open");
+			}
 		});
 		shinOSinstance.addApp("Afficher ma waifu","fa-file-image", function(){
 			shinOSinstance.exit();
