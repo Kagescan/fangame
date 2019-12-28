f9c111d1fa33d42ebaf877525a0c466ba193e9c3
LoganTann <logon313@hotmail.fr>
2019-12-28 23:22:53 +0100

    Add yaki speechs (not finished, just for fun)

---
+++ js/chapters/fr/yakiSpeechs.js
@@ -0,0 +1,63 @@
+// WARNING : spoil alert regarding to the character's name involved.
+// Read only if you completed the manga of Kagerou Daze
+
+let script = scriptFr;
+
+script["yaki_WhyDoYouThink"] = [
+  "yaki Hein ?",
+  "yaki C'est faux ?",
+  "shin Tout est faux !",
+  "shin Tu comprends vraiment rien à tout ce que je t'ai dit ?",
+  "yaki J'étais assez confiante pourtant !",
+  "yaki Peut être que tu m'as appris les mauvaises choses",
+  "shin Impossible !",
+  "shin Il y a juste quelque chose qui cloche avec ton cerveau !",
+  "yaki Vraiment ?",
+  "yaki Je suppose que c'est de ma faute alors...",
+  "yaki ...",
+  "yaki Hey, Shintaro !",
+  "shin Quoi ?",
+  "yaki Shintaro, penses-tu que le paradis existe ?",
+  "shin HAH ?",
+  "shin Bien sûr que non ! C'est juste un endroit imaginé par les humains.",
+  "shin Si cela existe vraiment, on aurait pu le prouver facilement !"
+  "yaki Je vois...",
+  "yaki Mais je pense",
+  "yaki Je pense qu'il existe vraiment.",
+  "shin ?",
+  "yaki Même si je ne pense pas que ce soit un paradis pour les bonnes personnes après leurs mort,",
+  "yaki Parce-que peu importe où l'on va, laisser quelqu'un est triste, non ?",
+  "shin Mais pourquoi parles-tu de ça ?",
+  "yaki Désolée, je vais arrêter de dire des trucs étranges...",
+  "yaki Je pense qu'on devrait continuer de réviser.<br> Je vais poser des questions et tu réponds, d'accord ?",
+  "yaki Okay, Shintaro",
+  "yaki _ _ _ _ _ _ _ _ ?",
+  "shin _ _ _ _ .",
+  "yaki C'est correct, comme on peut l'attendre de toi !",
+  "yaki Alors prochaine question !<br> _ _ _ _ _ ?",
+  "shin _ _ _ ...",
+  "shin Il n'y a aucun intéret à me poser des questions. Tu es celle qui devrait apprendre tout ça.",
+  "yaki AH ! Je suppose que tu as raison"
+  "yaki Alors laisse moi juste te poser quelque chose que toi seul tu sais.",
+  "yaki <span class='big'>Pourquoi penses tu que je suis morte ?</span>"
+]
+
+script["yaki_ItsBeenAwhile"] = [
+  "yaki Ça fait longtemps, n'est-ce pas ?",
+  "hm ?",
+  "yaki Non, comparé à autrefois, les chances de nous rencontrer sont plus nombreuses.",
+  "yaki De toute manière, je suis contente de te <span class='rem'>revoir</span>",
+  "Hein, de quoi veux-tu parler ?",
+  "yaki À en juger par ta réaction, tu as sûrement oublié ?",
+  "yaki C'est triste...",
+  "yaki Tu m'as pourtant dit autrefois que tu ne m'oublierais jamais.",
+  "Euh...",
+  "yaki He he... Excuse moi, excuse moi...<br>Ce n'est pas grave, tu n'es pas le seul.",
+  "yaki Tout le monde ne se souvient de moi que petit à petit",
+  "yaki Car il est impossible à qui que ce soit de me connaître parfaitement dès le départ.",
+  "Je ...",
+  "yaki Ta mémoire te revient-elle ?",
+  "yaki Bon, j'ai beaucoup de temps devant moi, alors ...",
+  "yaki Parlons encore !",
+  "yaki Je vais te raconter notre première rencontre."
+]
